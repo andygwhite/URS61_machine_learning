dbcb6e27fefad5bf07d32ee44401eb53941bf46b
andygwhite <None>
2022-11-30 18:11:01 -0800

    Removed debugging code, made output more clear

--- test_load.py
+++ test_load.py
@@ -9,26 +9,22 @@ import csvprocessing as csv
 import pandas as pd
 from numpy import genfromtxt
 
-model = torch.load('./model.pt')
+model = torch.load('./model_DEMO.pt')
 model.eval()
 
-process = csv.csvProcessor("/home/mininet/network_topo/labeled_datasets/validation/network.csv")
+process = csv.csvProcessor("/home/mininet/network_topo/labeled_datasets_DEMO/validation/all.csv")
 inputs, targets = process.datatoArr()
 inputs = (pd.get_dummies(pd.DataFrame(inputs), columns=[0,1,2,5]))
-inputs = (inputs / inputs.max(axis=0))[0:10]
-# TESTING!!
-inputs = pd.DataFrame([[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.15384615384615385, 0.9626666666666667, 0.0, 0.0, 1.0, 0.0],
-                       [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.07692307692307693, 0.09466666666666666, 0.0, 1.0, 0.0, 0.0]])
-# End testing
+inputs = (inputs / inputs.max(axis=0))
 correct = 0
 count = 0
 for in_arr, target_arr in zip(inputs.values, targets):
     pred = model(torch.from_numpy(in_arr).float()).data.numpy()
-    print(in_arr)
-    print(pred, pred.argmax())
+    # print(in_arr)
+    # print(pred, pred.argmax())
     count += 1
     if pred.argmax() == target_arr.argmax():
         correct += 1
-print(correct)
-print(count)
-print(correct/count)
+print("Num correct:", correct)
+print("Total count:", count)
+print("Accuracy:", 100*correct/count, "%")
